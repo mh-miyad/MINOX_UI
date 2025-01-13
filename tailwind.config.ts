9c5e7aef5a9482e5f59653c416f72d7a405917d5
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-01-13 18:52:14 +0600

    tailconfig changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
